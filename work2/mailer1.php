cb89373946c702d9361e243bbd23bef12e278b99
NtGolda <38866999+NtGolda@users.noreply.github.com>
2023-06-28 13:30:31 +0300

    mailer1.php

--- work2/mailer1.php
+++ work2/mailer1.php
@@ -1,39 +1,28 @@
 <?php
 
-namespace App\Services\User;
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
-//require '../../../vendor/autoload.php';
+require_once 'phpmailer\PHPMailer.php';
+require_once 'phpmailer\Exception.php';
+require_once 'phpmailer\SMTP.php';
 
-class SendMailService
-{
-    public function __construct()
-    {
-    }
 
-    public function send($user)
-    {
-        $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-        $mail->Host = 'ssl://smtp.rambler.ru';
-        $mail-> Port = 465;
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->SMTPAuth = true;
-        $mail->Username = 'testmailer08@rambler.ru';
-        $mail->Password = 'Passwordformailer08';
-        $mail->setFrom('testmailer08@rambler.ru', 'Instant Remember');
-        $mail->AddAddress("testaddress08@rambler.ru");
-        $mail->Subject = 'subject';
-        $mail->Body = 'body';
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Host = 'ssl://smtp.rambler.ru';
+$mail-> Port = 465;
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$mail->SMTPAuth = true;
+$mail->Username = 'testmailer08@rambler.ru';
+$mail->Password = 'Passwordformailer08';
+$mail->setFrom('testmailer08@rambler.ru', 'Instant Remember');
+$mail->AddAddress("testaddress08@rambler.ru");
+$mail->Subject = 'subject';
+$mail->Body = 'body';
 
-        if (!$mail-> send()) {
-            echo 'Mailer Error: '. $mail->ErrorInfo;
-        } else {
-            echo 'Message sent!';
-        }
-    }
+if (!$mail-> send()) {
+    echo 'Mailer Error: '. $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
 }
